91dc582b75edb5fc1fc484e1141234bd806d8ece
Harsh Virani <iamharshvirani7@gmail.com>
2020-05-18 02:34:26 -0700

    User Profile

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,6 +7,11 @@ import 'package:instagram_clone/screens/profile_screen.dart';
 import 'package:instagram_clone/screens/search_screen.dart';
 
 class HomeScreen extends StatefulWidget {
+
+  final String userId;
+
+  HomeScreen({this.userId});
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -43,7 +48,7 @@ class _HomeScreenState extends State<HomeScreen> {
           SearchScreen(),
           CreatePostScreen(),
           ActivityScreen(),
-          ProfileScreen(),
+          ProfileScreen(userId: widget.userId),
         ],
         onPageChanged: (int index) {
           setState(() {
